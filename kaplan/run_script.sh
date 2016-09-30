7744ecbb2874fbb58770543e67397e6fb5a85d76
Dan Sandiford <sonderfjord@gmail.com>
2016-10-01 08:32:50 +1000

    length scale test

--- kaplan/run_script.sh
+++ kaplan/run_script.sh
@@ -1,4 +1,4 @@
-for c in `seq 0.0001 0.0005 0.01`
-do
-   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 3 pd.fthickness=0.00775 pd.friction_mu=0.001 pd.friction_C=$c pd.friction_min=0.001 md.RES=128 md.elementType="Q1/dQ0" 
+for l in `seq 0.0015 0.0005 0.0155`
+do 
+   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 3 pd.fthickness=$l pd.friction_mu=0.001 pd.friction_C=0.0002 pd.friction_min=0.001 md.RES=128
 done
