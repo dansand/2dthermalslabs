83fe31740f01248d784257cfe2b6ab55f48d4557
Dan Sandiford <sonderfjord@gmail.com>
2016-10-01 12:22:37 +1000

    a few more lengths

--- kaplan/run_script.sh
+++ kaplan/run_script.sh
@@ -1,4 +1,4 @@
-for l in `seq 0.0015 0.0005 0.0155`
+for l in `seq 0.0005 0.0005 0.0205`
 do 
-   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 3 pd.fthickness=$l pd.friction_mu=0.001 pd.friction_C=0.0002 pd.friction_min=0.001 md.RES=128
+   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 4 pd.fthickness=$l pd.friction_mu=0.001 pd.friction_C=0.0002 pd.friction_min=0.001 md.RES=128
 done
