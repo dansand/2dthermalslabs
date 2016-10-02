f5a6051b2e76832922afac6050e6500cb1367013
Dan Sandiford <sonderfjord@gmail.com>
2016-10-02 20:33:22 +1100

    another length scale with different elements

--- kaplan/run_script.sh
+++ kaplan/run_script.sh
@@ -1,6 +1,6 @@
 for a in "Q1/dQ0" "Q2/dpc1"
 do
-   for for l in `seq 0.0005 0.0005 0.0205`
+   for l in `seq 0.0005 0.0005 0.0205`
    do
       docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 7 pd.fthickness=$l pd.friction_mu=0.001 pd.friction_C=0.0002 pd.friction_min=0.001 md.RES=128 md.elementType=$a
    done
