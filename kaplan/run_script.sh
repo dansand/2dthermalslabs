d9a73d2908e2d8350dab2eea484df5baf844b85b
Dan Sandiford <sonderfjord@gmail.com>
2016-10-04 11:08:14 +1100

    next model nectar

--- kaplan/run_script.sh
+++ kaplan/run_script.sh
@@ -1,7 +1,7 @@
-for e in 1.0 10. 100. 1000.
+for e in 5.0 10. 20. 50.
 do
-   for o in 30.0 45.0 60.0
+   for o in 15.0 30.0
    do
-      docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 8 pd.fthickness=0.0075 pd.friction_C=0.0002 md.RES=128 md.elementType="Q2/dpc1" pd.deltaViscosity=$e pd.orientation=$o
+      docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 9 pd.fthickness=0.0075 pd.friction_C=0.0002 md.RES=128 md.elementType="Q2/dpc1" pd.deltaViscosity=$e pd.orientation=$o
    done
 done
