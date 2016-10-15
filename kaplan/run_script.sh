8fe23ee79f12f811a821355dd528af70d29a244d
Dan Sandiford <sonderfjord@gmail.com>
2016-10-15 13:34:51 +1100

    model test fault width with viscosity change

--- kaplan/run_script.sh
+++ kaplan/run_script.sh
@@ -1,7 +1,4 @@
-for a in "Q1/dQ0"
+for l in `seq 0.0005 0.0005 0.0205`
 do
-   for l in `seq 0.0005 0.0005 0.0205`
-   do
-      docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 10 pd.fthickness=$l pd.friction_mu=0.001 pd.friction_C=0.0002 pd.friction_min=0.001 md.RES=256 md.elementType=$a
-   done
-done
+   docker run -v $PWD:/workspace  -i -t --rm dansand/underworld2-dev mpirun -np 32 python Fault_BuoyancyDriven-devel.py 11 pd.fthickness=$l pd.friction_C=0.0002 md.RES=128 md.elementType="Q2/dpc1" pd.deltaViscosity=20 pd.orientation=30
+done 
