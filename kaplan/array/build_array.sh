90bcfdfbfb913b952b54b14ada252221dda7ac7f
Dan Sandiford <sonderfjord@gmail.com>
2016-10-19 02:28:13 +1100

    hot fixes x2

--- kaplan/array/build_array.sh
+++ kaplan/array/build_array.sh
@@ -6,9 +6,12 @@ do
    do
       for c in 0.75 1.0
       do
-        qsub -v COUNTER=$counter,A=$a,B=$b,C=$c array.pbs
-        let counter=counter+1
-        #if [ "$counter" -gt 1 ]; then break 3; fi;
+        for d in 0.5 1.0 2.0
+        do
+           qsub -v COUNTER=$counter,A=$a,B=$b,C=$c,D=$d array.pbs
+           let counter=counter+1
+           #if [ "$counter" -gt 1 ]; then break 3; fi;
+        done
       done
    done
 done
