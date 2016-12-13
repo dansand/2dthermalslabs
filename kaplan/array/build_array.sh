305eaafb9f059e290b0e9600b613d4bd70831b69
Dan Sandiford <sonderfjord@gmail.com>
2016-12-13 18:21:27 +1100

    array job

--- kaplan/array/build_array.sh
+++ kaplan/array/build_array.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 counter=1
-for a in 0.25 0.5 1.0 2.0 4.0
+for a in 0.1 0.25 0.5 1.0 2.0 5.0 10.0
 do
-   for b in 0.25 0.5 1.0 2.0 4.0
+   for b in 0.1 0.25 0.5 1.0 2.0 5.0 10.0
    do
-      for c in 0.1 0.25 0.5 1.0 2.0
-      do
-         for d in 0.1 0.25 0.5 1.0
-         do
-            #qsub -v COUNTER=$counter,A=$a,B=$b,C=$c,D=$d array.pbs
-            echo $counter $a $b $c $d
-            let counter=counter+1
-            if [ "$counter" -gt 1 ]; then break 4; fi;
-         done
-      done
+      #qsub -v COUNTER=$counter,A=$a,B=$b,C=$c,D=$d array.pbs
+      echo $counter $a $b $c $d
+      let counter=counter+1
+      if [ "$counter" -gt 1 ]; then break 2; fi;
    done
 done
--- kaplan/array/old/build_array.sh
+++ kaplan/array/old/build_array.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 counter=1
-for a in 0.2 1.0 2.0
+for a in 0.25 0.5 1.0 2.0 4.0
 do
-   for b in 0.5 1.0 2.0 4.0
+   for b in 0.25 0.5 1.0 2.0 4.0
    do
-      for c in 0.5 1.0 2.0
+      for c in 0.1 0.25 0.5 1.0 2.0
       do
-         for d in 0.1 1.0 10.0
-         do 
-            qsub -v COUNTER=$counter,A=$a,B=$b,C=$c,D=$d array.pbs
+         for d in 0.1 0.25 0.5 1.0
+         do
+            #qsub -v COUNTER=$counter,A=$a,B=$b,C=$c,D=$d array.pbs
+            echo $counter $a $b $c $d
             let counter=counter+1
+            if [ "$counter" -gt 1 ]; then break 4; fi;
          done
       done
    done
 done
-
